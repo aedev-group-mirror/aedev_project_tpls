0e0190288a96f607b811fc0c9a4e070294480195
AndiEcker <aecker2@gmail.com>
2025-11-13 15:00:38 +0000

    v0.3.66: added the patched_shutdown_wrapper fixture to the conftest.py template
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/project_tpls/__init__.py
    M  aedev/project_tpls/templates/tests/de_otf_conftest.py
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt
    M  tests/test_project_tpls.py

--- aedev/project_tpls/templates/tests/de_otf_conftest.py
+++ aedev/project_tpls/templates/tests/de_otf_conftest.py
@@ -68,6 +68,32 @@ def cons_app(restore_app_env):
 
 
 @pytest.fixture
+def patched_shutdown_wrapper():
+    """ log :func:`ae.console.ConsoleApp.shutdown` function calls and args, while preventing exit/quit of main app. """
+    exit_call_args = []
+
+    class _ExitCaller(Exception):
+        """ exception to recognize and simulate app shutdown for the code to be tested. """
+
+    def _exit_(*args, **kwargs):
+        # nonlocal exit_call_args
+        exit_call_args.append((args, kwargs))
+        raise _ExitCaller("to be caught by the _call_wrapper() of the patched_shutdown_wrapper unit test fixture")
+
+    def _call_wrapper(fun, *args, **kwargs):
+        exit_call_args.clear()
+        try:
+            ret = fun(*args, **kwargs)
+        except _ExitCaller:
+            ret = None
+        print(f"patched_shutdown_wrapper._call_wrapper {ret=}")
+        return exit_call_args
+
+    with patch('ae.console.ConsoleApp.shutdown', new=_exit_):
+        yield _call_wrapper
+
+
+@pytest.fixture
 def tst_system(cons_app):
     """ CURRENTLY NOT USED """
     # pylint: disable=import-outside-toplevel, no-name-in-module, import-error
