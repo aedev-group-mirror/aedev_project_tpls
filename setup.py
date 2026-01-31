2edcc9094c94ba6b6cc5cb5e15e8fc514a983f0a
AndiEcker <aecker2@gmail.com>
2026-01-31 17:06:36 +0000

    v0.3.75: extended conftest tpl with log of failed tests to skip further integration test runs after failure
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/project_tpls/__init__.py
    M  aedev/project_tpls/templates/tests/de_otf_conftest.py
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.73
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.74
 """ setup of aedev namespace package portion project_tpls: managed Python project files templates. """
 import sys
 # noinspection PyUnresolvedReferences
@@ -99,7 +99,7 @@ setup_kwargs = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_project_tpls',
-    'version': '0.3.74',
+    'version': '0.3.75',
     'zip_safe': False,
 }
 
