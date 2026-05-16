522238aa0e5ccdf2ee90123e84cfbd93c1d0b9d2
AndiEcker <aecker2@gmail.com>
2026-05-16 12:58:11 +0100

    v0.3.78: refactored after split-out of system helpers from ae-base into new ae-app-log and ae-system portions
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/project_tpls/__init__.py
    M  aedev/project_tpls/templates/tests/de_otf_conftest.py
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt

--- aedev/project_tpls/templates/tests/de_otf_conftest.py
+++ aedev/project_tpls/templates/tests/de_otf_conftest.py
@@ -10,8 +10,9 @@ from unittest.mock import patch
 import pytest
 
 
+# noinspection PyTypeChecker
 assert (cwd := os.getcwd()) == (prj_root := os.path.dirname(os.path.dirname(__file__))), f"wrong {cwd=}, != {prj_root=}"
-sys.path.insert(0, prj_root)  # add project root (==CWD) to sys.path (to run pytest w/o the 'python -m' prefix)
+sys.path.insert(0, prj_root)  # add project root (==CWD) to sys.path (allowing to run pytest w/o the 'python -m' prefix)
 
 
 SKIP_EXPRESSION = "'CI_PROJECT_ID' in os.environ"
@@ -130,7 +131,7 @@ def logging_unpatched_shutdown_teardown() -> LoggedShutdownCalls:
 
 def _log_shutdown_calls(main_app: Any, exit_code: int, error_message: str = ""):
     """ AppBase|ConsoleApp.shutdown()-mock to prevent unregister of main-app|template-projects to log them in tests. """
-    from ae.base import stack_frames
+    from ae.system import stack_frames
 
     print()
     print(f"***** app.shutdown() called with {exit_code=} and {error_message=}; {main_app=}; stack:")
@@ -231,4 +232,4 @@ def tst_system(cons_app):
     """ CURRENTLY NOT USED """
     # pylint: disable=import-outside-toplevel, no-name-in-module, import-error
     from ae.sys_core import SystemBase      # type: ignore
-    yield SystemBase('Tst', cons_app, dict(User='TstUsr', Password='TstPwd', Dsn='TstDb@TstHost'))
+    yield SystemBase('Tst', cons_app, dict(User='TstUsr', Password='Tst Pwd', Dsn='TstDb@TstHost'))
