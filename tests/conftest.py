464004a9fcdb0c0471099a19c82ffce4ca33cf5f
AndiEcker <aecker2@gmail.com>
2026-07-14 16:18:50 +0100

    v0.3.81: enhanced formatting of shutdown call log in conftest-template
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/project_tpls/__init__.py
    M  aedev/project_tpls/templates/tests/PutMar-conftest.py
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.79
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.80
 # pylint: disable=redefined-outer-name, unused-argument; suppress fixtures conflicts (silly pylint)
 """ fixtures for to test this project """
 import os
