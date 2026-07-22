0a91fa2340603c4019d5bf6d05cb7727b7cc39bc
AndiEcker <aecker2@gmail.com>
2026-07-22 17:39:11 +0100

    v0.3.86: added missing hyphens to pip version 2nd check in .gitlab-ci.yml and its template
    M  .gitlab-ci.yml
    M  README.md
    M  aedev/project_tpls/__init__.py
    M  aedev/project_tpls/templates/fSt-PutMar-.gitlab-ci.yml
    M  setup.py

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup_kwargs: dict[str, Any] = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_project_tpls',
-    'version': '0.3.85',
+    'version': '0.3.86',
     'zip_safe': False,
 }
 
