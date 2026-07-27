4dbe6a715722dfeffb9c411f0e1093e1678093d5
AndiEcker <aecker2@gmail.com>
2026-07-27 17:29:57 +0100

    v0.3.88: replaced pip install option ignore-installed with force-reinstall in .gitlab-ci.yml-template
    M  .gitignore
    M  .gitlab-ci.yml
    D  .readthedocs.yaml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/project_tpls/__init__.py
    M  aedev/project_tpls/templates/fSt-PutMar-.gitlab-ci.yml
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.83
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.87
 """ setup of aedev namespace package portion project_tpls: managed Python project files templates. """
 import pathlib
 import sys
@@ -94,7 +94,7 @@ setup_kwargs: dict[str, Any] = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_project_tpls',
-    'version': '0.3.87',
+    'version': '0.3.88',
     'zip_safe': False,
 }
 
