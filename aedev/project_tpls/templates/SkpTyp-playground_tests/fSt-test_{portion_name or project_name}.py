d3208067a0ae36d6ce9f47be68403ffb57070829
AndiEcker <aecker2@gmail.com>
2026-08-22 14:02:03 +0100

    v0.3.99: extended .gitlab-ci.yml-template to allow projects with optional unit tests
    useful for playground project type (without a tests/TESTS_FOLDER folder in the project/repository root folder)
    
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/project_tpls/__init__.py
    M  "aedev/project_tpls/templates/SkpTyp-playground_tests/fSt-test_{portion_name or project_name}.py"
    M  aedev/project_tpls/templates/fSt-PutMar-.gitlab-ci.yml
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt

--- aedev/project_tpls/templates/SkpTyp-playground_tests/fSt-test_{portion_name or project_name}.py
+++ aedev/project_tpls/templates/SkpTyp-playground_tests/fSt-test_{portion_name or project_name}.py
@@ -1,6 +1,12 @@
 """ {portion_name or project_name} unit tests. """
 
-# ReplaceWith#(from {os_path_relpath(version_file, project_path)[:-3].replace("/", '.')} import __version__, __doc__)#
+# ReplaceWith#(from {os_path_relpath(version_file, project_path)[:-3].replace("/", '.')} import __doc__, __version__)#
+
+
+def test_docstring():
+    """ test existence of this {project_type} project docstring. """
+    assert __doc__
+    assert isinstance(__doc__, str)
 
 
 def test_version():
@@ -8,9 +14,3 @@ def test_version():
     assert __version__
     assert isinstance(__version__, str)
     assert __version__.count(".") == 2
-
-
-def test_docstring():
-    """ test existence of this {project_type} project docstring. """
-    assert __doc__
-    assert isinstance(__doc__, str)
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.97
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.98
 """ setup of aedev namespace package portion project_tpls: managed Python project files templates. """
 import pathlib
 import sys
@@ -75,8 +75,8 @@ setup_kwargs: dict[str, Any] = {
             'templates/SkpPor-docs/fSt-PutMar-requirements.txt',
             'templates/SkpPor-docs/PutMar-build_docs.sh',
             'templates/SkpPor-docs/fSt-PutMar-conf.py',
-            'templates/SkpTyp-playground_tests/fSt-PutMar-requirements.txt',
             'templates/SkpTyp-playground_tests/PutMar-conftest.py',
+            'templates/SkpTyp-playground_tests/fSt-PutMar-requirements.txt',
             'templates/SkpTyp-playground_tests/fSt-test_{portion_name or project_name}.py',
         ],
     },
@@ -94,7 +94,7 @@ setup_kwargs: dict[str, Any] = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_project_tpls',
-    'version': '0.3.98',
+    'version': '0.3.99',
     'zip_safe': False,
 }
 
